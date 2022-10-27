b0fbdaae30a70e6b6bf81a6f88d3ad1ec54cbb15
Dongboy111 <2508205368@qq.com>
2022-10-27 18:58:55 +0800

    加了点动作空间信息

--- env/BaseEnv.py
+++ env/BaseEnv.py
@@ -1,12 +1,14 @@
 from typing import Tuple, Optional, Union, List
-
+from gym import spaces
 import gym
 from gym.core import RenderFrame
 
 
 class BaseEnv(gym.Env):
     def __init__(self):
-        super.__init__()
+        super(BaseEnv,self).__init__()
+        self.action_space = spaces.Discrete(100)
+        self.observation_space = spaces.Discrete(1000)
     def step(self, action: list) -> Tuple[list, float, bool, bool, dict]:
         pass
     def __getstate__(self):
