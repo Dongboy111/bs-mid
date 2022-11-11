18b46518c2c6e30771956312684474a4c25482e8
Dongboy111 <2508205368@qq.com>
2022-11-11 17:27:22 +0800

    2022-11-11

--- utils/CommonCal.py
+++ utils/CommonCal.py
@@ -3,15 +3,27 @@
 """
 import math
 
+
+
 from res.CommonArgs import *
+
+
 
 #获取Ee
 def getEe(ETheta,n):
     return ETheta + (2303*R*T)/(n*F)
 
-#腐蚀单位计算
-def getCorrosionPoint(alpha,Ic_h0,n,E,Ee):
+#腐蚀单位阴极计算计算
+def getCorrosionPointC(alpha, Ic_h0, n, E, Ee):
     Ic_h = Ic_h0*math.exp(-(alpha*n*F*(E-Ee))/(R*T))
     Ec = -0.1183*math.log(Ic_h,10)-1.05259
+
     return Ec
+#腐蚀单位阳极计算计算
+def getCorrosionPointM(alpha:float, Ic_m0:list, n:int, E:list, Ee:list):
+    Ia_m = Ic_m0 * math.exp((alpha*n*F*(E-Ee))/(R*T))
+
+def getEcorr():
+    return
+
 
